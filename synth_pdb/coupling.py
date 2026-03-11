db2dc48c8d3f7458380fd1dc3d0809df51d57095
George Elkins <george.f.elkins@gmail.com>
2026-03-10 20:10:54 -0400

    Require synth-nmr>=0.8.0 to incorporate the latest NMR simulation engine improvements (fix unit test failure)

--- synth_pdb/coupling.py
+++ synth_pdb/coupling.py
@@ -6,6 +6,11 @@ For direct usage of NMR functionality, consider using synth-nmr directly:
     pip install synth-nmr
     from synth_nmr import calculate_hn_ha_coupling, predict_couplings_from_structure
 
+The J-coupling (scalar coupling) is a fundamental NMR parameter that provides
+information about the chemical bonding and local geometry of a molecule.
+In proteins, the 3J(HN-HA) coupling is particularly useful as it relates to the
+phi torsion angle via the Karplus equation.
+
 See: https://github.com/elkins/synth-nmr
 """
 
